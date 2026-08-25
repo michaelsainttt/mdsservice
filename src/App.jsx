ef8e599e7c6b2ed7a957b7c0bb04cf30c8aed845
michaelsainttt <michaelsaint2005@gmail.com>
2026-08-25 14:10:59 -0400

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import About from './Pages/About'
+
+function App() {
+
+  return (
+    <>
+      <About />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/About.jsx
@@ -0,0 +1,17 @@
+function About(){
+    return(
+        <main>
+            <section>
+                <img src="Assets\TruckInHorizon1.jpg" alt="Truck In Horizon"></img>
+                <h1>About Us</h1>
+                <p>
+                We support independent drivers with reliable freight,
+                responsive service, and transparent operations.
+                </p>
+            </section>
+            <section>
+
+            </section>
+        </main>
+    )
+}
